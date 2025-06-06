98c19217a5aa3d34f2975b02114d00017eadf775
sambit <sambitmishra2024+github@gmail.com>
2025-06-06 11:34:38 +0530

    add getHumanChoice

--- script.js
+++ script.js
@@ -14,3 +14,11 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice())
+
+
+function getHumanChoice () {
+    let choice = prompt("enter rock, paper or scissors")
+    return choice.toLowerCase()
+}
+
+console.log(getHumanChoice())
