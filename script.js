b30296f84ac6951ac371025a8c7729ce6e8c6853
sambit <sambitmishra2024+github@gmail.com>
2025-06-06 11:30:59 +0530

    add getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello")
+console.log("hello:welcome to Rock-Paper-Scissors")
+
+function getComputerChoice() {
+    let val = Math.floor((Math.random() * 3) + 1)
+    if (val == 1){
+        return "rock"
+    }
+    else if(val == 2) {
+        return "paper"
+    }
+    else{
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice())
